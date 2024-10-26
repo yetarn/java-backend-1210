5f73123c7fb7553dab131db3a0f6d48ab649cddd
ytrnby <yetar2004@gmail.com>
2024-10-26 13:16:41 +0400

    Nese commit

--- src/main/java/az/turingacademy/Main.java
+++ src/main/java/az/turingacademy/Main.java
@@ -2,6 +2,24 @@ package az.turingacademy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte a=10;
+        short b=15;
+        int c=78;
+        long d=10000L;
+        float e=123.23f;
+        double f=2098.987;
+        char g='A';
+        boolean h=true;
+
+        System.out.println ("Byte is " + a);
+        System.out.print ("Short is " + b +"\n");
+        System.out.printf ("Int is " + c + "\n");
+        System.out.println ("Long is " + d );
+        System.out.print ("Float is " + e +"\n");
+        System.out.printf ("Double is " + f + "\n");
+        System.out.print ("Char is " + g +"\n");
+        System.out.printf ("Boolean is " + h + "\n");
+
     }
+
 }
